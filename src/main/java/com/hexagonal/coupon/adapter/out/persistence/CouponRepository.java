b849df8b6f894f211f00d4ced59b084609659ab8
JeonSangYong <tkddyd420@naver.com>
2023-02-16 15:00:49 +0900

    feat : 쿠폰, 회원, 회원쿠폰 jpa repository

---
+++ src/main/java/com/hexagonal/coupon/adapter/out/persistence/CouponRepository.java
@@ -0,0 +1,6 @@
+package com.hexagonal.coupon.adapter.out.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+interface CouponRepository extends JpaRepository<CouponJpaEntity, Long> {
+}
---
+++ src/main/java/com/hexagonal/coupon/adapter/out/persistence/MemberCouponRepository.java
@@ -0,0 +1,6 @@
+package com.hexagonal.coupon.adapter.out.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+interface MemberCouponRepository extends JpaRepository<MemberCouponJpaEntity, Long> {
+}
