f01fb093a8a8331d49c6fb62c992a18891808e94
JeonSangYong <tkddyd420@naver.com>
2023-02-16 12:27:26 +0900

    feat : 쿠폰 발급 웹 어댑터

---
+++ src/main/java/com/hexagonal/coupon/adapter/in/web/MemberCouponApiController.java
@@ -0,0 +1,21 @@
+package com.hexagonal.coupon.adapter.in.web;
+
+import com.hexagonal.coupon.application.port.in.CreateMemberCouponCommand;
+import com.hexagonal.coupon.application.port.in.CreateMemberCouponUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+class MemberCouponApiController {
+
+    private final CreateMemberCouponUseCase createMemberCouponUseCase;
+
+    @PostMapping("/members/{memberId}/coupons/{couponId}")
+    public boolean createMemberCoupon(@PathVariable Long memberId,
+                                      @PathVariable Long couponId) {
+        return createMemberCouponUseCase.createMemberCoupon(new CreateMemberCouponCommand(memberId, couponId));
+    }
+}
